6c2f0e59a06c0e3853d91657e85a26405d3cca51
Chavesito <leo.favio2003@gmail.com>
2025-01-29 19:52:30 -0500

    Error solve
    error solve

--- styles/PantallaInicio.styles.js
+++ styles/PantallaInicio.styles.js
@@ -12,20 +12,11 @@ const styles = StyleSheet.create({
     height: 75,
     marginBottom: 20,
   },
-<<<<<<< Updated upstream
   mascot: {
     width: 250,
     height: 350,
     marginBottom: 20
   },
-=======
-
-  image: {
-    width: 210,
-    height: 300,
-    marginBottom: 20,
-
->>>>>>> Stashed changes
   description: {
     fontSize: 24,
     fontWeight: 'bold',
